4bdce24eb9cd3e4c931b4583e167200f994e34b5
divyativrekar0409-droid <divyativrekar0409@gmail.com>
2026-03-13 13:21:32 +0000

    GlobalExceptionHandler class

--- incident-api/src/main/java/com/support/incident_api/exception/GlobalExceptionHandler.java
+++ incident-api/src/main/java/com/support/incident_api/exception/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
+//for handling exception 
 @ControllerAdvice
 public class GlobalExceptionHandler{
 
